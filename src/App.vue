59800e0444785cb777f613b5eacec4765a0f2ec4
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-11 18:09:13 +0300

    style: add card styles

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,13 @@ import { RouterLink, RouterView } from 'vue-router'
     </header>
 
     <div class="options-container">
-
+      <div class="card">
+        <img alt="Math image" class="logo" src="@/assets/images/math.jpeg"/>
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 questions</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,5 +42,35 @@ header {
 #search-quiz {
   border: none;
   background-color: rgba(128, 128, 128, 0.1);
+}
+
+.options-container{
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 2rem;
+}
+
+.card {
+  width: 20rem;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 3rem;
+  margin-right: 2rem;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 12rem;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 1rem;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
 }
 </style>
