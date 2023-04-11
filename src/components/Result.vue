3b02d393d9231859484cd2789d61e23e117a1667
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-11 22:08:23 +0300

    feat: add result props

---
+++ src/components/Result.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {RouterLink} from 'vue-router';
+const {numberOfCorrectAnswers, quizQuestionLength} = defineProps(["numberOfCorrectAnswers", "quizQuestionLength"])
+</script>
+
+<template>
+    <div class="results">
+        <p id="result-text">Your results...</p>
+        <h1 id="result">{{numberOfCorrectAnswers}}/{{quizQuestionLength}}</h1>
+    </div>
+    <RouterLink to="/">Go Back</RouterLink>
+</template>
+
+<style scoped>
+.results {
+    text-align: center;
+    padding: 5rem 0;
+}
+
+#result-text {
+    font-size: 5rem;
+}
+
+#result {
+    font-size: 10rem;
+}
+
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
+import Result from "../components/Result.vue";
 import { useRoute } from "vue-router";
 import quizzes from "../assets/data/quizzes.json"
 import { computed } from "@vue/reactivity";
@@ -27,8 +28,14 @@ const onOptionSelected = (isCorrect) => {
   if (isCorrect) {
     numberOfCorrectAnswers++
   }
+
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true  
+  }
+
   currentQuestionIndex.value++
 }
+const showResults = ref(false)
 
 </script>
 
@@ -36,7 +43,8 @@ const onOptionSelected = (isCorrect) => {
   <div>
     <QuizHeader :barPercentage="barPercentage" :questionStatus="questionStatus"/>
     <div>
-      <Question :question="quiz.questions[currentQuestionIndex]" @selectOption="onOptionSelected" />
+      <Question v-if="!showResults" :question="quiz.questions[currentQuestionIndex]" @selectOption="onOptionSelected" />
+      <Result v-else :quizQuestionLength="quiz.questions.length" :numberOfCorrectAnswers="numberOfCorrectAnswers"/>
     </div>
   </div>
 </template>
