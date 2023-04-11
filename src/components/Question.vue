4384518ee9242c886d744a485c9ec12abac4bf62
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-11 21:34:17 +0300

    feat: emit selectOption event

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,6 +1,11 @@
 <script setup>
+
+const emit = defineEmits(["selectOption"])
  const {question} = defineProps(['question'])
 
+const emitSelectedOption = (isCorrect) => {
+  emit("selectOption", isCorrect)
+}
 </script>
 
 <template>
@@ -10,7 +15,7 @@
             {{ question.text }}
           </h1>
           <div class="answers-container">
-            <div v-for="option in question.options" :key="option.id" class="answer">
+            <div v-for="option in question.options" :key="option.id" class="answer" @click="emitSelectedOption(option.isCorrect)">
               <p class="answer-label">{{ option.lable }}</p>
               <div class="answer-value">
                 <p>{{ option.text }}</p>
